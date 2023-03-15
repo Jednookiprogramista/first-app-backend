00a43e1d1cf3afecb2ab28b4b1664f4dc95e82b8
Your Name <bvbrunik@gmail.com>
2023-03-15 20:01:17 +0100

    Created errors validation

---
+++ utils/errors.ts
@@ -0,0 +1,18 @@
+import {Response, Request, NextFunction} from "express";
+
+export class ValidationError extends Error {
+
+}
+
+export const handleError  = () => (err: Error,req: Request,res: Response,next: NextFunction) => {
+    console.log(err);
+
+    res
+        .status(err instanceof ValidationError ? 400 : 500)
+        .json({
+        message: err instanceof ValidationError ? err.message : 'Sorry,please try again later',
+
+
+    });
+
+}
